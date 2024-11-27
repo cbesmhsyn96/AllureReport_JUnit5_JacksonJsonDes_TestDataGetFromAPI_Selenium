b3001959f511cf350b014e8a73464137ddbba915
cbesmhsyn96 <system.c.hsyn@gmail.com>
2024-11-27 20:26:22 +0300

    Junit5 @Order annotation is added, POM approach approached

--- src/test/java/tests/TestHomePage.java
+++ src/test/java/tests/TestHomePage.java
@@ -1,6 +1,7 @@
 package tests;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import utils.ScreenshotOnFailureExtension;
@@ -10,6 +11,7 @@ import java.io.IOException;
 @ExtendWith(ScreenshotOnFailureExtension.class)
 public class TestHomePage extends StepsOfTests{
     @Test
+    @Order(1)
     @DisplayName("Allthingsdev web sitesine url ile gidilmesi")
     void goWebSiteByUrlTest() throws IOException {
         goToUrl("https://www.allthingsdev.co/");
@@ -22,12 +24,31 @@ public class TestHomePage extends StepsOfTests{
      */
 
     @Test
+    @Order(2)
     @DisplayName("Navbarda Book Demo linkine tıklanması")
     void bookDemoNavbarLinkClick() throws IOException {
         goToUrl("https://www.allthingsdev.co/");
         clickElementByKey("BookDemo_NavbarBtn");
         switchToFrameByKey("BookDemoIframe");
         waitAndAssertEqualElementText("AllThingsDev | API Booking Page","BookDemoIframe_Text");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Navbarda Search linkine tıklanması")
+    void searchNavbarLinkClick() throws IOException {
+        goToUrl("https://www.allthingsdev.co/");
+        clickElementByKey("Search_NavbarBtn");
+        waitAndAssertEqualElementText("Search:","SearchText");
+    }
+
+    @Test
+    @Order(4)
+    @DisplayName("Navbarda API Catalogue linkine tıklanması")
+    void apiCatalogueNavbarLinkClick() throws IOException {
+        goToUrl("https://www.allthingsdev.co/");
+        clickElementByKey("APICatalogue_NavbarBtn");
+        waitAndAssertEqualElementText("All API Categories","AllAPICategoriesText");
     }
 
 
